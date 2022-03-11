18515430ae233d19a2f2a51f0419011bbaa811c0
FirstBroRussia <chipanrussia@gmail.com>
2022-03-11 17:49:02 +0300

    Мелкая стилизация,шрифты

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,38 @@
-import '../less/styles.less';
+import "../less/styles.less";
 
-console.log('aaaaaaaaaaaa');
+let isHidden = true;
+let currentSelectedItem = null;
+
+const selectField = document.querySelector(".select-field");
+const inputElement = document.querySelector(".input");
+const listElement = document.querySelector(".list");
+
+selectField.addEventListener("click", (evt) => {
+  if (isHidden) {
+    isHidden = !isHidden;
+    console.log(listElement);
+    listElement.classList.remove('display-none');
+  } else {
+    isHidden = !isHidden;
+    listElement.classList.add('display-none');
+  }
+});
+
+listElement.addEventListener("click", (evt) => {
+  const targetElement = evt.target.closest(".item");
+  if (targetElement) {
+    const valueTargetElement =
+      targetElement.querySelector(".value").textContent;
+    inputElement.value = valueTargetElement;
+    textContentTargetElement = targetElement.innerText;
+    selectField.textContent = textContentTargetElement;
+    selectField.classList.add("selected-text");
+    if (currentSelectedItem !== null) {
+      currentSelectedItem.classList.remove("selected-text");
+    }
+    currentSelectedItem = targetElement;
+    targetElement.classList.add("selected-text");
+    isHidden = !isHidden;
+    listElement.classList.add('display-none');
+  }
+});
